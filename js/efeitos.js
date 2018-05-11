4da9305fd543781d1cc1372edb20cd054760f56e
JGabriel <joaogbsi@gmail.com>
2018-05-11 14:52:49 -0300

    manha 11052018

--- js/efeitos.js
+++ js/efeitos.js
@@ -44,5 +44,17 @@ $(document).ready(function(){
 		}, 1000);
 
 		event.preventDefault();
-	})
+	});
+
+	$("#btn-bars").on("click", function(){
+		$("header").toggleClass("open-menu")
+	});
+
+	$("#menu-mobile-mask, .btn-close").on("click", function(){
+		$("header").removeClass("open-menu")
+	});
+
+	$("#btn-search").on("click", function(){
+		$("header").toggleClass("open-menu")
+	});
 });
