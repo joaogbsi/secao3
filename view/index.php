8670f38d1130f9e820baec0e2077ee9816961add
JGabriel <joaogbsi@gmail.com>
2018-05-28 11:15:19 -0300

    manha 28052018_2

---
+++ view/index.php
@@ -0,0 +1,127 @@
+<?php
+	include_once("header.php");
+?>
+
+		<section>
+			<div id="banner">
+				<h1>
+					Orlando City
+					<small>Orlando City Soccer Club</small>
+				</h1>
+
+			</div>
+
+			<div id="news" class="container">
+				<div class="row text-center">
+					<h2>Latest news</h2>
+					<hr>					
+				</div>
+
+				<button type="button" id="btn-news-prev"><i class="fa fa-angle-left"></i></button>
+				<button type="button" id="btn-news-next"><i class="fa fa-angle-right"></i></button>
+				<div class="row thumbnails owl-carousel owl-theme">
+					<div class="item">
+						<div class="item-inner">
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik form Toronto FC</h3>
+							<time>December 21, 2015</time>							
+						</div>
+					</div>
+					<div class="item">
+						<div class="item-inner">
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik form Toronto FC</h3>
+							<time>December 21, 2015</time>							
+						</div>
+					</div>
+					<div class="item">
+						<div class="item-inner">
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik form Toronto FC</h3>
+							<time>December 21, 2015</time>							
+						</div>
+					</div>
+					<div class="item">
+						<div class="item-inner">
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik form Toronto FC</h3>
+							<time>December 21, 2015</time>							
+						</div>
+					</div>
+					<div class="item">
+						<div class="item-inner">
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik form Toronto FC</h3>
+							<time>December 21, 2015</time>							
+						</div>
+					</div>
+					<div class="item">
+						<div class="item-inner">
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik form Toronto FC</h3>
+							<time>December 21, 2015</time>							
+						</div>
+					</div>
+					<div class="item">
+						<div class="item-inner">
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik form Toronto FC</h3>
+							<time>December 21, 2015</time>							
+						</div>
+					</div>
+					<div class="item">
+						<div class="item-inner">
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik form Toronto FC</h3>
+							<time>December 21, 2015</time>							
+						</div>
+					</div>
+				</div>
+			</div><!-- container -->
+
+			<div id="estatisticas">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<p>61348<small>Stadium Capacity</small></p>
+						</div>
+						<div class="col-md-4">
+							<p>2010<small>Founded</small></p>
+						</div>
+						<div class="col-md-4">
+							<p>7th<small>Eastern Coference</small></p>
+						</div>
+					</div>
+				</div><!-- container -->
+					
+			</div><!-- estatisticas -->
+
+			<div id="call-to-action">
+				<div class="container">
+
+					<div class="row text-center">
+						<h2>American club number one in Brazil</h2>
+						<hr>
+					</div>
+
+					<div class="row">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur aliquid sunt iusto quae. Vero facilis qui autem quia minima cupiditate, ratione corporis officiis consequatur quasi, enim rem excepturi laudantium exercitationem!</p>
+					</div>
+
+					<div class="row row-max-400">
+						<div class="col-xs-6">
+							<a href="#" class="btn btn-roxo">Shop</a>
+						</div>
+						<div class="col-xs-6">
+							<a href="#" class="btn btn-amarelo">Register</a>
+						</div>
+					</div>
+					
+				</div>
+				
+			</div><!--call-to-action-->
+		</section>
+
+<?php
+	include_once("footer.php");
+?>		
